20f9f117ed881241421012842a43adf98377c9fc
Luiz Lelis <luizhlelis@gmail.com>
2021-04-12 16:58:14 -0300

    chore: worker listening to queue

---
+++ src/Worker/Program.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+
+namespace Worker
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        private static IConfigurationRoot BuildAppConfiguration()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            return configuration.AddEnvironmentVariables().Build();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var configuration = BuildAppConfiguration();
+
+                    services.AddHostedService<WorkerBackgroundService>();
+
+                    var factory = new ConnectionFactory() { HostName = configuration["RabbitMQ:Url"] };
+                    services.AddSingleton(factory);
+                });
+    }
+}
---
+++ src/Worker/WorkerBackgroundService.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Worker
+{
+    public class WorkerBackgroundService : BackgroundService
+    {
+        private readonly ILogger<WorkerBackgroundService> _logger;
+        private readonly IConfiguration _configuration;
+        private readonly ConnectionFactory _connectionFactory;
+
+        public WorkerBackgroundService(
+            ILogger<WorkerBackgroundService> logger,
+            IConfiguration configuration,
+            ConnectionFactory connectionFactory)
+        {
+            _logger = logger;
+            _configuration = configuration;
+            _connectionFactory = connectionFactory;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            using (var connection = _connectionFactory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: _configuration["RabbitMQ:QueueName"], durable: false,
+                        exclusive: false, autoDelete: false, arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body.ToArray();
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: "hello", autoAck: true, consumer: consumer);
+                }
+            }
+
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+
+        public override async Task StopAsync(CancellationToken stoppingToken)
+        {
+            _logger.LogInformation("Stopping queue listener...");
+            await base.StopAsync(stoppingToken);
+        }
+    }
+}
